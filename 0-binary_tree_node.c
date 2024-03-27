a20c151890a802ae0850bcbc8f9496ca7919077a
ben obame <obameben@yahoo.fr>
2024-03-27 12:49:28 +0200

    task 0

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -15,7 +15,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	new_node = malloc(sizeof(binary_tree_t));
 	if (new_node == NULL)
 		return (NULL); /* Memory allocation failed */
-	
+
 	/* Initialize the new node */
 	new_node->n = value;
 	new_node->parent = parent;
