07ac97d5c3606ab0332dc8ca031b00bc7e049177
ben obame <obameben@yahoo.fr>
2024-03-27 15:38:08 +0200

    task 18

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_uncle - finds the uncle of a node
+ * @node: pointer to the node to find the uncle
+ * Return: pointer to the uncle node
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	binary_tree_t *parent, *grandparent;
+
+	if (!node || !node->parent || !node->parent->parent)
+		return (NULL);
+
+	parent = node->parent;
+	grandparent = parent->parent;
+
+	if (grandparent->left == parent)
+		return (grandparent->right);
+	else
+		return (grandparent->left);
+}
