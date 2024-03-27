e7165541763e1a24f7fa98b62e627cc175449692
ben obame <obameben@yahoo.fr>
2024-03-27 15:26:37 +0200

    task 15

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,9 +1,9 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_full - checks if a binary tree is full
+ * binary_tree_is_full - Checks if a binary tree is full
  * @tree: pointer to the root node of the tree to check
- * Return: 1 if the tree is full, 0 otherwise
+ * Return: 1 if the tree is full, 0 otherwise or if tree is NULL
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
@@ -13,5 +13,8 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	if (!tree->left && !tree->right)
 		return (1);
 
-	return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+	if (tree->left && tree->right)
+		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+
+	return (0);
 }
